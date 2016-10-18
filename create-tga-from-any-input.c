1b5fb7171e43546afa4f70d7b21d0732aedc5b86
vipyne <vipyne@gmail.com>
2016-10-18 15:47:29 -0400

    initial commit

--- create-tga-from-any-input.c
+++ create-tga-from-any-input.c
@@ -11,8 +11,7 @@
 #include <string.h>
 
 #define BYTE_RANGE 256
-//#define WIDTH 250
-//#define HEIGHT 250
+#define RGBA 3
 
 ////// targa file header
 
@@ -54,6 +53,7 @@ int big_endianify (int number)
 	return number / BYTE_RANGE;
 }
 
+// used if sorting the pixels
 int compare_function (const void* a_pointer, const void* b_pointer)
 {
 	return *(( char* )a_pointer) - *(( char* )b_pointer);
@@ -97,7 +97,7 @@ int main (int argc, char* argv[])
 	targa_header header;       // variable of targa_header type
 
 	int x, y;                  // coordinates for `for` loops to pass in
-		    		 		   // correct number of values
+		    		 		   // correct number of pixel values
 
 	int HEIGHT = atoi(argv[3]);
 	int WIDTH = atoi(argv[3]);
@@ -125,122 +125,85 @@ int main (int argc, char* argv[])
 
 	write_header(header, tga);
 
+	printf("^^^^ header written\n");
+	
+	// source input file
 	FILE *source;
 	source = fopen(argv[1], "rb");
-	fseek(source, 100, SEEK_END);
+	fseek(source, 0, SEEK_END);
 	int source_size = ftell(source);
-	//char *buffer;
-	//size_t size;
-
-	//int pipe_fd_arr[2]; // pipe_fd_arr[0] == stdin
-
-	//FILE *stream = fdopen(source, 
-
-
-	printf("asdfa before stream asdf \n");
-	//stream = open_memstream(&buffer, &size);
-	//printf(stream, source);
-
-	printf("asdf asdf \n");
-
-	printf("source size %d\n", source_size);
-
 	rewind(source);
-	printf("ftell after rewind: %ld\n", ftell(source));
 
-	int input_binary_length = 3 * WIDTH * HEIGHT; // normal people call this a buffer
+	printf("^^^^ source file read,      length: %d\n", source_size);
 	
+	// width * height = number of pixels
+	// number of pixels * 3 for each pixel's R G B value (no Alpha at this point)
+	int input_binary_length = WIDTH * HEIGHT * RGBA; // normal people call this a buffer
+	
+
+	
+			printf("bef normsup\n");	
+	// buffer for pixel values (no zeros)
 	char normalized_input[input_binary_length];
 	
+			printf("before readtrhuuuuusup\n");	
+	// buffer for entire input file
 
-	char read_through[source_size];;
-	//char* read_through = malloc(sizeof(char));
-	char* read_through_2 = malloc(sizeof(char));
+	char read_through[source_size];
 
-	//read_through[	] = SEEK_END;
+	//char *read_through;
 
+	//read_through = (char *) malloc (sizeof(char)*source_size);
+	//read_through = (char *) calloc ( source_size, sizeof(char) );
+	//read_through[source_size] = '\0';;
+
+			printf("afterrrsup\n");	
 	int i = 0;
 	int read_through_index = 0;
-	int second_index = 0;
-
-	//fflush(source);
 
 	while (i < input_binary_length) 
-	{
-		if (false)
-		//if (read_through_index > 1005820)
-		{	
-			printf("i--- %d\n", i);
-			//printf("inputbinarylen- %d\n", input_binary_length);
-			fread(&read_through_2, 1, 1, source);	
-			//normalized_input[i] = 1;
-			//i++;
-			if (read_through_2[second_index] == EOF || read_through_2[second_index] == '\0')
-			{	
-				printf("EOF \n");
-				rewind(source);
-			}
-			if (read_through_2[second_index] != 0)
-			{	
-				printf("second-index %d \n", second_index);
-				normalized_input[i] = read_through_2[second_index];
-			//	printf("while if %c, %d\n", normalized_input[i], i);
-				i++;
-			}
-			second_index++;
-			
-		} else {
-			
-			//printf("while\n");
-			fread(&read_through, 1, source_size, source);
-			if (i == source_size)	
-				printf("i == source size   %d \n", i);
-			
-			if (read_through_index == source_size)	
-				printf("read_through_index == source size   %d \n", read_through_index);
-			//if (read_through[read_through_index] == EOF)
-			//if (read_through[read_through_index] == EOF || read_through[read_through_index] == '\0')
-			//printf("------ %d \n", read_through_index);
-			//{
-			//	printf("EOF \n");
-			//	rewind(source);
-			//}
-			if (read_through[read_through_index] != '0')
-			{	
-				normalized_input[i] = read_through[read_through_index];
-				//printf("while if %c, %d\n", normalized_input[i], i);
-				i++;
-			}
-			//printf("after second if %d\n", read_through_index);
-		
-			read_through_index++;
-		}
-	}
-	printf("final read thorugh index %d\n", read_through_index);
-	printf("final i %d\n", i);
+	{	
+			//printf("sasdfup\n");	
+		fread(&read_through, 1, source_size, source);
+			//printf("read through \n");
 
-	printf("source size %d\n", source_size);
+
+	
+			//printf("rt rti : %d\n", read_through[read_through_index]);	
+		if (read_through[read_through_index] != '0')
+		{
+			//printf("sup\n");	
+			normalized_input[i] = read_through[read_through_index];
+			i++;
+		}
+		read_through_index++;
+	}
+	//free(read_through);
+	
+	printf("^^^^ normalized buffer set, length: %d \n", (int) sizeof(normalized_input));
+	
 	//// magic happens here -- write the pixels
 
-	// qsort(normalized_input, strlen(normalized_input), 1, compare_function);
+	char normalized_sorted[input_binary_length];
+	strncpy(normalized_sorted, normalized_input, input_binary_length);
+	
+	qsort(normalized_sorted, strlen(normalized_input), 1, compare_function);
 	
 	int n_index = 0;
 	for (int y = 0; y < HEIGHT; ++y)
 	{
 		for (int x = 0; x < WIDTH; ++x)
 		{
-		//fread(&read_through, 1, 1, source);	
-			//fputc(normalized_input[n_index], stdout);
-			fputc(normalized_input[x], tga);
-			//n_index++;
-			//fputc(normalized_input[n_index], stdout);
-			fputc(normalized_input[y], tga);
-			//n_index++;
-			//fputc(normalized_input[n_index], stdout);
-			fputc(normalized_input[n_index/255], tga);
-		// B G R order
+			// pixels read in B G R order
+			fputc(normalized_input[n_index], tga);
+			fputc(read_through[n_index], tga);
+			fputc(normalized_sorted[n_index], tga);
+		
 			n_index++;
-			if (x % 2 == 0)
+			
+
+
+	if (x % 2 == 0)
 			//for (int inner = 0; inner < y; ++inner)
 			{
 				//fputc(normalized_input[x]-y, tga);
