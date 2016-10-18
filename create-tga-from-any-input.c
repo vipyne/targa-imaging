6f5ce220a84d4cc7019be906660b5fd9efa5d07b
vipyne <vipyne@gmail.com>
2016-10-18 16:43:17 -0400

    tab space

--- create-tga-from-any-input.c
+++ create-tga-from-any-input.c
@@ -23,29 +23,29 @@
 ////// targa file header
 
 typedef struct {
-	char id_length;       // length of id field (number of bytes - max 255)
-	char map_type;        // colormap field (0 or 1; no map or 256 entry palette)
-	char image_type;			// ( 0 - no image data included
-												//   1 - uncompressed, color mapped image
+char id_length;       // length of id field (number of bytes - max 255)
+char map_type;        // colormap field (0 or 1; no map or 256 entry palette)
+char image_type;			// ( 0 - no image data included
+//   1 - uncompressed, color mapped image
 //	  2 - uncompressed, RGB image
 //	  3 - uncompressed, black & white image
 //	  9 - run-length encoded(RLE-lossless compression),color mapped image
 //	 10 - RLE, RGB image
 //	 11 - compressed, black & white image )
 
-	int map_first;       // first entry index for color map
-	int map_length;      // total number of entries in color map
-	char map_entry_size; // number of bits per entry
+int map_first;       // first entry index for color map
+int map_length;      // total number of entries in color map
+char map_entry_size; // number of bits per entry
 
-	int x;               // x cooridinate of origin
-	int y;               // y cooridinate of origin
+int x;               // x cooridinate of origin
+int y;               // y cooridinate of origin
 
-	int width;           // width in pixels
-	int height;          // height in pixels
+int width;           // width in pixels
+int height;          // height in pixels
 
-	char bits_per_pixel; // number of bits per pixel
+char bits_per_pixel; // number of bits per pixel
 
-	char misc;           // srsly? "scan origin and alpha bits" this example uses scan origin
+char misc;           // srsly? "scan origin and alpha bits" this example uses scan origin
 											 // honestly, don't know what's going on here. we pass in a hex value
 											 // :shrug_emoji:
 } targa_header;
@@ -57,13 +57,13 @@ int little_endianify (int number)
 
 int big_endianify (int number)
 {
-	return number / BYTE_RANGE;
+return number / BYTE_RANGE;
 }
 
 // used if sorting the pixels
 int compare_function (const void* a_pointer, const void* b_pointer)
 {
-	return *(( char* )a_pointer) - *(( char* )b_pointer);
+return *(( char* )a_pointer) - *(( char* )b_pointer);
 }
 
 ////// write header function
